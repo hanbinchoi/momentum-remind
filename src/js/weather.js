e631d4f5e6ceaee7573e4b521aa60706998dee1e
hanbinchoi <wkghskak@naver.com>
2023-04-28 13:47:40 +0900

    hide

--- src/js/weather.js
+++ src/js/weather.js
@@ -2,7 +2,7 @@ function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   console.log(lat, lon);
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${env.API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
